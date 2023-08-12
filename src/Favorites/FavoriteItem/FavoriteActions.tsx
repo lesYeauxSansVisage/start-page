819f4ace32cd6fbcef22363d7df3470a0139be51
Gen <genesishawkeye42@gmail.com>
2023-08-12 10:57:08 -0300

    FEAT: Added FavoriteActions component

---
+++ src/Favorites/FavoriteItem/FavoriteActions.tsx
@@ -0,0 +1,44 @@
+import "./FavoriteActions.scss";
+
+type FavoriteActionsProps = {
+  onEdit: () => void;
+  onClose: () => void;
+  onDelete: () => void;
+};
+
+const FavoriteActions = ({
+  onEdit,
+  onClose,
+  onDelete,
+}: FavoriteActionsProps) => {
+  const editFavoriteHandler = () => {
+    onEdit();
+    onClose();
+  };
+
+  const deleteFavoriteHandler = () => {
+    onDelete();
+  };
+  
+
+  return (
+    <div className="favorite__actions">
+      <button
+        className="favorite__edit-button button"
+        onClick={editFavoriteHandler}
+      >
+        Edit
+      </button>
+      <button
+        className="favorite__delete-button button"
+        onClick={deleteFavoriteHandler}
+      >
+        Delete
+      </button>
+      <button className="favorite__cancel-button button" onClick={onClose}>
+        Cancel
+      </button>
+    </div>
+  );
+};
+export default FavoriteActions;
