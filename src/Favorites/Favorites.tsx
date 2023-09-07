ff276ea7e7a87af9fbe1076d164810db5ad1bad5
Gen <genesishawkeye42@gmail.com>
2023-09-07 09:37:54 -0300

    FEAT: Added context to Favorite component

--- src/Favorites/Favorites.tsx
+++ src/Favorites/Favorites.tsx
@@ -1,88 +1,30 @@
+import ReactDOM from "react-dom";
+
 import FavoritesImage from "./FavoritesImage/FavoritesImage";
 import "./Favorites.scss";
 import FavoriteItem from "./FavoriteItem/FavoriteItem";
-import { useState } from "react";
-import FavoritesForm, { FormValues } from "./FavoritesForm/FavoritesForm";
-
-export type Favorite = {
-  id: number;
-  name: string;
-  url: string;
-  icon: string;
-};
-
-const testFav: Favorite[] = [
-  {
-    id: 1,
-    name: "rf 301",
-    url: "http://10.0.1.1/login.html",
-    icon: "fa-solid fa-wifi",
-  },
-  {
-    id: 2,
-    name: "rf 302",
-    url: "http://10.0.1.1/login.html",
-    icon: "fa-solid fa-wifi",
-  },
-  {
-    id: 3,
-    name: "rf 303",
-    url: "http://10.0.1.1/login.html",
-    icon: "fa-solid fa-wifi",
-  },
-  {
-    id: 4,
-    name: "rf 304",
-    url: "http://10.0.1.1/login.html",
-    icon: "fa-solid fa-wifi",
-  },
-  {
-    id: 5,
-    name: "rf 305",
-    url: "http://10.0.1.1/login.html",
-    icon: "fa-solid fa-wifi",
-  },
-  {
-    id: 6,
-    name: "rf 306",
-    url: "http://10.0.1.1/login.html",
-    icon: "fa-solid fa-wifi",
-  },
-  {
-    id: 7,
-    name: "rf 307",
-    url: "http://10.0.1.1/login.html",
-    icon: "fa-solid fa-wifi",
-  },
-  {
-    id: 8,
-    name: "rf 308",
-    url: "http://10.0.1.1/login.html",
-    icon: "fa-solid fa-wifi",
-  },
-  {
-    id: 9,
-    name: "rf 309",
-    url: "http://10.0.1.1/login.html",
-    icon: "fa-solid fa-wifi",
-  },
-];
+import { useState, useContext } from "react";
+import FavoritesForm from "./FavoritesForm/FavoritesForm";
+import { FavoriteType } from "../interfaces/IFavorite";
+import FavoritesContext from "../context/favorites-context";
 
 function Favorites() {
-  const [favorites, setFavorites] = useState<Favorite[]>(testFav);
+  const favContext = useContext(FavoritesContext);
 
   const [editMode, setEditMode] = useState(false);
 
-  let editData: Favorite | null = null;
+  let editData: FavoriteType | null = null;
 
   const [formVisibility, setFormVisibility] = useState(false);
 
   const [editId, setEditId] = useState(-1);
 
-  const showAddButton = favorites.length < 10;
+  const showAddButton = favContext?.favorites.length! < 10;
 
   if (editMode) {
-    editData = favorites.find((favorite) => favorite.id === editId) as Favorite;
+    editData = favContext?.favorites.find(
+      (favorite: FavoriteType) => favorite.id === editId
+    ) as FavoriteType;
   }
 
   const closeFormHandler = () => {
@@ -98,25 +40,6 @@ function Favorites() {
     setFormVisibility(true);
   };
 
-  const addFavorite = (formValues: FormValues) => {
-    setFavorites((prevFavorites) => {
-      const newFavorite: Favorite = {
-        id: favorites.length > 0 ? favorites[favorites.length - 1].id + 1 : 1,
-        name: formValues.name,
-        url: formValues.url,
-        icon: formValues.icon,
-      };
-
-      return [...prevFavorites, newFavorite];
-    });
-  };
-
-  const deleteFavorite = (id: number) => {
-    setFavorites((prevFavorites) => {
-      return prevFavorites.filter((favorite) => favorite.id !== id);
-    });
-  };
-
   const editFavoriteHandler = (id: number) => {
     setEditMode(true);
     setEditId(id);
@@ -124,37 +47,20 @@ function Favorites() {
     setFormVisibility(true);
   };
 
-  const editFavorite = (id: number, updatedData: FormValues) => {
-    setFavorites((prevFavorites) => {
-      return prevFavorites.map((favorite) => {
-        if (favorite.id == id) {
-          return {
-            ...favorite,
-            name: updatedData.name,
-            url: updatedData.url,
-            icon: updatedData.icon,
-          };
-        }
-
-        return favorite;
-      });
-    });
-  };
-
   return (
     <div className="favorites">
-      {formVisibility && (
-        <FavoritesForm
-          onClose={closeFormHandler}
-          onAdd={addFavorite}
-          editMode={editMode}
-          editData={editData}
-          onEdit={editFavorite}
-        />
-      )}
+      {formVisibility &&
+        ReactDOM.createPortal(
+          <FavoritesForm
+            onClose={closeFormHandler}
+            editMode={editMode}
+            editData={editData}
+          />,
+          document.body
+        )}
       <FavoritesImage></FavoritesImage>
       <div className="favorites__items">
-        {favorites.map((favorites) => {
+        {favContext?.favorites.map((favorites: FavoriteType) => {
           return (
             <FavoriteItem
               key={favorites.id}
@@ -162,7 +68,6 @@ function Favorites() {
               name={favorites.name}
               url={favorites.url}
               icon={favorites.icon}
-              onDelete={deleteFavorite}
               onEdit={editFavoriteHandler}
             ></FavoriteItem>
           );
