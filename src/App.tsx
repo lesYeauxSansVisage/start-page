75583204d62119f9c9227e023f107f41d4c4f75b
Gen <genesishawkeye42@gmail.com>
2023-09-13 19:20:27 -0300

    FEAT: Added Todos to the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import "./App.scss";
 import Favorites from "./Favorites/Favorites";
+import Todos from "./Todos/Todos";
 import { FavoritesProvider } from "./context/favorites-context";
 
 function App() {
   return (
     <div className="container">
       <FavoritesProvider>
-        <Favorites></Favorites>
+        <Favorites />
       </FavoritesProvider>
+
+      <Todos />
     </div>
   );
 }
