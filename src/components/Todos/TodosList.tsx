081238c8da8a630b7d7eef4a7aa8cc570183f41b
Gen <genesishawkeye42@gmail.com>
2023-09-18 17:23:56 -0300

    FEAT: Added props to receive todos array

--- src/components/Todos/TodosList.tsx
+++ src/components/Todos/TodosList.tsx
@@ -1,11 +1,19 @@
+import { TodoType } from "../../interfaces/TodoType";
 import TodoItem from "./TodoItem";
 import "./TodosList.scss";
 
-const TodosList = () => {
+type TodosListProps = {
+  todos: TodoType[];
+};
+
+const TodosList = (props: TodosListProps) => {
   return (
     <ul className="todos-list">
-      <TodoItem />
+      {props.todos.map((todo) => (
+        <TodoItem key={todo.id} todo={todo} />
+      ))}
     </ul>
   );
 };
+
 export default TodosList;
