666c718f07343be836c69995ff2033af8d18264e
Gen <genesishawkeye42@gmail.com>
2023-09-18 17:25:11 -0300

    FEAT: Added props to receive todo

--- src/components/Todos/TodoItem.tsx
+++ src/components/Todos/TodoItem.tsx
@@ -1,10 +1,19 @@
+import { TodoType } from "../../interfaces/TodoType";
 import "./TodoItem.scss";
 
-const TodoItem = () => {
+type TodoItemProps = {
+  todo: TodoType;
+};
+
+const TodoItem = (props: TodoItemProps) => {
+  const deleteHandler = () => {
+    console.log("You clicked on the todo with the id: " + props.todo.id);
+  };
+
   return (
     <li className="todo">
-      <span className="todo__text">Remember to not remember anything</span>
-      <button className="todo__delete-button">
+      <span className="todo__text">{props.todo.text}</span>
+      <button className="todo__delete-button" onClick={deleteHandler}>
         <i className="fa-solid fa-eraser"></i>
       </button>
     </li>
