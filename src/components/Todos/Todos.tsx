fdf1ba88f187e7309775918a7274bfca22ed8483
Gen <genesishawkeye42@gmail.com>
2023-09-20 18:22:08 -0300

    FEAT: Added mock todos

--- src/components/Todos/Todos.tsx
+++ src/components/Todos/Todos.tsx
@@ -1,9 +1,18 @@
 import { useState } from "react";
 import "./Todos.scss";
 import TodosList from "./TodosList";
+import { TodoType } from "../../interfaces/TodoType";
+
+const DUMMY_TODOS: TodoType[] = [
+  { id: 1, text: "Make chores", completed: true, priority: "low" },
+  { id: 2, text: "Read book", completed: false, priority: "medium" },
+  { id: 3, text: "Make bed", completed: true, priority: "high" },
+  { id: 4, text: "Wash dishes", completed: false, priority: "low" },
+  { id: 5, text: "Clean houses", completed: false, priority: "high" },
+];
 
 const Todos = () => {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState<TodoType[]>(DUMMY_TODOS);
 
   return (
     <div className="todos">
@@ -15,7 +24,7 @@ const Todos = () => {
         />
       </form>
 
-      <TodosList />
+      <TodosList todos={todos} />
     </div>
   );
 };
