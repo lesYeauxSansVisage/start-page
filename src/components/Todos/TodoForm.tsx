03ffe28f19691861ff77c7728babacb2bd978d09
Gen <genesishawkeye42@gmail.com>
2023-09-20 18:21:29 -0300

    FEAT: Added the TodoForm component

---
+++ src/components/Todos/TodoForm.tsx
@@ -0,0 +1,13 @@
+const TodoForm = () => {
+  return (
+    <form>
+      <input
+        type="text"
+        placeholder="Write a todo..."
+        className="todos__input"
+      />
+    </form>
+  );
+};
+
+export default TodoForm;
