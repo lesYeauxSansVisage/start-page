f4c5893f1a7dead2c1c5dde869c188fdc52c9092
Gen <genesishawkeye42@gmail.com>
2023-09-21 16:41:37 -0300

    REFACTOR: Removed Todos import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import "./App.scss";
 import Favorites from "./components/Favorites/Favorites";
-import Todos from "./components/Todos/Todos";
 import { FavoritesProvider } from "./context/favorites-context";
 
 function App() {
